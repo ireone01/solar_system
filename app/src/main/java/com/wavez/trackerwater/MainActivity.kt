e39b98043973071ef78e3591e0238fbc41016849
ireone01 <huyvip10a3@gmail.com>
2024-12-30 17:37:53 +0700

    comit

--- app/src/main/java/com/wavez/trackerwater/MainActivity.kt
+++ app/src/main/java/com/wavez/trackerwater/MainActivity.kt
@@ -1,10 +1,23 @@
 package com.wavez.trackerwater
 
+import android.os.Bundle
 import com.lingvo.base_common.ui.BaseActivity
 import com.wavez.trackerwater.databinding.ActivityMainBinding
 
 class MainActivity: BaseActivity<ActivityMainBinding>() {
     override fun createBinding(): ActivityMainBinding {
         return ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun initConfig(savedInstanceState: Bundle?) {
+        super.initConfig(savedInstanceState)
+    }
+
+    override fun initObserver() {
+        super.initObserver()
+    }
+
+    override fun initListener() {
+        super.initListener()
     }
 }
